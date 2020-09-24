8075f049c85fe322d2d5ad73b3accb02742595e0
Lyla Tsu <tsul@gilmour.org>
2020-09-24 10:41:15 -0400

    test motion

--- student.py
+++ student.py
@@ -62,7 +62,7 @@ class Piggy(PiggyParent):
         
         #lower-ordered example..
         self.right(primary=50, counter=-50)
-        self.back
+        self.back()
         time.sleep(2)
         self.stop()
 

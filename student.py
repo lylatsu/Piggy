da05108f79c0f8ff3cadd48eda905b305e3cac0e
Lyla Tsu <tsul@gilmour.org>
2020-09-24 10:53:42 -0400

    test motion

--- student.py
+++ student.py
@@ -65,7 +65,6 @@ class Piggy(PiggyParent):
         time.sleep(.5)
         self.back()
         time.sleep(2)
-        self.stop()
         turn_by_deg(90)
         self.sleep(2)
         self.stop()

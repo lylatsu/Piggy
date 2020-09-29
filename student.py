538eb5cd59303bc8dd75ea6a5ce7400b860b7181
Lyla Tsu <tsul@gilmour.org>
2020-09-29 10:39:55 -0400

    safety check

--- student.py
+++ student.py
@@ -58,7 +58,8 @@ class Piggy(PiggyParent):
 
     def dance(self):
         """A higher-ordered algorithm to make your robot dance"""
-        # TODO: check to see if it's safe before dancing
+        if not self.safe_to_dance():
+            return False # SHUT THE DANCE DOWN
         
         #lower-ordered example..
         
@@ -108,7 +109,16 @@ class Piggy(PiggyParent):
             
     def safe_to_dance(self):
         """Does a 360 distance check and returns true if safe"""
-        pass
+        # check for all fail/early-termination conditions
+        for x in range(4):
+            if self.read_distance() < 300:
+                print("NOT SAFE TO DANCE!")
+                return False
+            else:
+                self.turn_by_deg(90)
+        # after all checks have been done. We deduce it's safe
+        print("SAFE TO DANCE!")
+        return True
 
     def shake(self):
         self.deg_fwd(720)

9ae1b02be27a15b27b085942c7122c2b2753883b
Lyla Tsu <tsul@gilmour.org>
2020-10-13 10:58:43 -0400

    move left and right

--- student.py
+++ student.py
@@ -197,6 +197,33 @@ class Piggy(PiggyParent):
         # sort the scan data for easier analysis
         self.scan_data = OrderedDict(sorted(self.scan_data.items()))
 
+    def right_or_left(self):
+        """ Should I turn left or right? Returns a 'r' or 'l' based on scan data """
+        self.scan()
+        
+        right_sum = 0
+        right_avg = 0
+        left_sum = 0
+        left_avg = 0
+        
+        for angle in range(0, self.scan_data):
+            # average up the distances on the right side
+            if angle < self.MIDPOINT
+                right_sum += self.scan_data[angle]
+                right_avg += 1
+            else:
+                left_sum += self.scan_data[angle]
+                left_avg += 1
+
+        # calculate averages
+        left_avg = left_sum / left_avg
+        right_avg + right_sum / right_avg
+
+        if left_avg > right_avg:
+            return 'l'
+        else:
+            return 'r'
+    
     def obstacle_count(self):
         """Does a 360 scan and returns the number of obstacles it sees"""
         # do a scan of the area in front of the robot
@@ -254,7 +281,11 @@ class Piggy(PiggyParent):
         while True:
             if not self.quick_check():
                 self.stop()
-                self.turn_until_clear()
+                # self.turn_until_clear()
+                if 'l' in self.right_or_left():
+                    self.turn_by_deg(-45)
+                else:
+                    self.turn_by_deg(45)
             else:
                 self.fwd()
         

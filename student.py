f589fd14d3cd120c2c804c82638b3031134459ae
Lyla Tsu <tsul@gilmour.org>
2020-10-15 11:11:35 -0400

    speed up head scan

--- student.py
+++ student.py
@@ -191,7 +191,7 @@ class Piggy(PiggyParent):
 
     def scan(self):
         """Sweep the servo and populate the scan_data dictionary"""
-        for angle in range(self.MIDPOINT-350, self.MIDPOINT+350, 15):
+        for angle in range(self.MIDPOINT-350, self.MIDPOINT+350, 20):
             self.servo(angle)
             self.scan_data[angle] = self.read_distance()
         # sort the scan data for easier analysis

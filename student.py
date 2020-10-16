2f1b393ce4d6e263880e2ab14c6eb2336e2cebd5
Lyla Tsu <tsul@gilmour.org>
2020-10-16 11:07:44 -0400

    change midpoint for mr a's robot

--- student.py
+++ student.py
@@ -22,7 +22,7 @@ class Piggy(PiggyParent):
         self.RIGHT_DEFAULT = 80
         self.SAFE_DISTANCE = 300
         self.CLOSE_DISTANCE = 30
-        self.MIDPOINT = 1525  # what servo command (1000-2000) is straight forward for your bot?
+        self.MIDPOINT = 1500  # what servo command (1000-2000) is straight forward for your bot?
         self.set_motor_power(self.MOTOR_LEFT + self.MOTOR_RIGHT, 0)
         self.load_defaults()
         
